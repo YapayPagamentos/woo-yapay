6e8b08f52be1032ca5b91274c4cc5c81af910d05
wagnerjaccoud <wagner.jaccoud@gmail.com>
2019-10-13 15:54:14 -0300

    Update class-wc-yapay_intermediador-bankslip-gateway.php

--- woo-yapay/class-wc-yapay_intermediador-bankslip-gateway.php
+++ woo-yapay/class-wc-yapay_intermediador-bankslip-gateway.php
@@ -253,7 +253,28 @@ class WC_Yapay_Intermediador_Bankslip_Gateway extends WC_Payment_Gateway {
             $params["transaction[shipping_type]"] = $shipping_type;
             $params["transaction[shipping_price]"] = $order->order_shipping;
         }
-        $params["transaction[price_discount]"] = $order->discount_total;
+        
+	// OBTER DESCONTOS ( MELHORA COMPATIBILIDADE DO PLUGIN PARA SITES QUE USAM TAXAS NEGATIVAS E TAMBÉM INCLUI USO DE CUPONS //
+		$the_order = wc_get_order( $order_id );
+
+		// BUSCAR DESCONTO APLICADOS NO PEDIDO
+		foreach( $the_order->get_items('fee') as $item_id => $item_fee ){
+
+    		// NOME DO DESCONTO
+   		 $fee_name = $item_fee->get_name();
+
+    		// VALOR TOTAL DO DESCONTO
+    		$fee_total = $item_fee->get_total();
+
+    		// VALOR TOTAL DA TAXA COM DESCONTO
+    		$fee_total_tax = $item_fee->get_total_tax();
+		}
+		$fee_total = abs($fee_total);
+		$fee_total = $fee_total + $the_order->get_total_discount();
+		$params["transaction[price_discount]"] = $fee_total;
+	// FIM OBTER DESCONTOS ( MELHORA COMPATIBILIDADE DO PLUGIN PARA SITES QUE USAM TAXAS NEGATIVAS E TAMBÉM INCLUI USO DE CUPONS //
+	    
+	//$params["transaction[price_discount]"] = $order->discount_total;
         $params["transaction[url_notification]"] = $this->get_wc_request_url($order_id);
         $params["transaction[available_payment_methods]"] = "6";
         
