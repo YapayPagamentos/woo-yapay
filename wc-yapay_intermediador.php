c6c6c4bf417a7308c19ece17e23eb9ebb8469590
Lucas Gama <lucas_tgama@hotmail.com>
2024-04-30 11:25:48 -0300

    fix: Alteração da versão do plugin de 0.7.2 para 0.7.3

--- wc-yapay_intermediador.php
+++ wc-yapay_intermediador.php
@@ -5,7 +5,7 @@
  * Description: Intermediador de pagamento Vindi para a plataforma WooCommerce.
  * Author: Integração Vindi Intermediador
  * Author URI: https://vindi.com.br/
- * Version: 0.7.2
+ * Version: 0.7.3
  * Text Domain: vindi-pagamento
  */
 
