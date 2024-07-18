ca013adc043141fa1973b859170f1e80b48d8839
Lucas Gama <lucas_tgama@hotmail.com>
2024-07-18 13:27:39 -0300

    Fix: conflitos de merges

--- templates/wc_yapay_intermediador_bolepix_form.php
+++ templates/wc_yapay_intermediador_bolepix_form.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-<script src="https://static.traycheckout.com.br/js/finger_print.js" type="text/javascript"></script>
 
 <fieldset id="wc-yapay_intermediador-bolepix-payment-form" class="wc_yapay_intermediador_gateway" data-yapay="payment-form">
 
     <input type="hidden" id="tcbPaymentMethod" name="wc-yapay_intermediador-bolepix-payment-method" class="required-entry" value="28" autocomplete="off">
+    <input type="hidden" name="finger_print" class="yapay_finger_print" data-enviroment="<?php echo esc_attr($enviroment); ?>">
     <?php if ($not_require_cpf == 'no') : ?>
         <div id="cpf_yapayBP" class="cpf_yapay" style="display: none">
             <label>CPF<strong style="color: red;">*</strong> (somente números)</label>
@@ -19,10 +19,3 @@ if ( ! defined( 'ABSPATH' ) ) {
     <div class="clear"></div>
 
 </fieldset>
-
-<script>
-    var fp = window.yapay.FingerPrint({
-        env: 'production'
-    });
-    document.getElementById('finger_print').value = fp.getFingerPrint();
-</script>
